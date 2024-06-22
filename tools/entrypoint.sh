92855cff180226352e1c8e442ec5aa0229e52426
eQuantum <8466209+eq19@users.noreply.github.com>
2024-06-22 20:07:14 +0700

    Delete tools/entrypoint.sh

--- tools/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-#gcloud auth/credential_file_override activate-service-account project-owner@feedmapping.iam.gserviceaccount.com
-gcloud config set account project-owner@feedmapping.iam.gserviceaccount.com
-gcloud info
